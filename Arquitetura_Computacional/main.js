8dce77282f54804c12ffc4a2def1eb60fb2d2bd2
SamuelSimonatto12c <samuel.correia@sptech.school>
2026-04-01 11:46:55 -0300

    mudando o nome do banco

--- Arquitetura_Computacional/main.js
+++ Arquitetura_Computacional/main.js
@@ -8,7 +8,7 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
@@ -19,10 +19,10 @@ const serial = async (
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: 'HOST_DO_BANCO',
-            user: 'USUARIO_DO_BANCO',
-            password: 'SENHA_DO_BANCO',
-            database: 'DATABASE_DO_BANCO',
+            host: 'localhost',
+            user: 'THERMUS',
+            password: 'urubu100',
+            database: 'thermus',
             port: 3306
         }
     ).promise();
@@ -63,7 +63,7 @@ const serial = async (
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO medida (sensor_Temperatura, sensor_Umidade) VALUES (?, ?)',
+                'INSERT INTO leitura (temperatura, umidade) VALUES (?, ?)',
                 [sensorTemperatura, sensorUmidade]
             );
             console.log("valores inseridos no banco: ", sensorTemperatura + ", " + sensorUmidade);
